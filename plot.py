3de5ea564e70a5c5fdc46bbabd7105228ef17099
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-02-27 16:00:57 +0100

    project mostly finished

--- plot.py
+++ plot.py
@@ -5,29 +5,28 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('convnet.csv')
 
 # Set the global font size
-plt.rcParams.update({'font.size': 16})
+plt.rcParams.update({'font.size': 12})
 
 # Set the size of the plot
-plt.figure(figsize=(20, 12))
+plt.figure(figsize=(8, 5))
 
 # Separate data based on 'Approx. Algo'
 algorithms = df['Approx. Algo'].unique()
 
-
 # Plot each algorithm separately
 for algo in algorithms:
     algo_data = df[df['Approx. Algo'] == algo]
-    plt.scatter(algo_data['Bit'], algo_data['Total Energy'], label=algo, s=12)
-    plt.plot(algo_data['Bit'], algo_data['Total Energy'], label=algo, linewidth=0.5)
+    #plt.scatter(algo_data['Bit'], algo_data['Total Energy'], s=50, alpha=0.7)  # dots at data points
+    line, = plt.plot(algo_data['Bit'], algo_data['Total Energy'], label=algo, linewidth=1, linestyle='dashed', marker='o')
 
 # Set labels and title
 plt.xlabel('Number of approximate bits')
-plt.ylabel('Total Energy[pJ]')
+plt.ylabel('Total Energy [pJ]')
 plt.title('Total energy for varying number of approximate bits for multiple algorithms')
 
-# Display legend
-plt.legend()
+# Create a separate legend for the lines
+handles, labels = plt.gca().get_legend_handles_labels()
+line_legend = plt.legend(handles, labels, loc='lower left')
 
 # Save the plot to a file
-plt.savefig('convnet.png')
-
+plt.savefig('convnet_new.png')
--- plotty.py
+++ plotty.py
@@ -5,33 +5,33 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('testout1.csv')
 
 # Set the figure size
-fig, ax1 = plt.subplots(figsize=(20, 12))
+fig, ax1 = plt.subplots(figsize=(8, 5))
 
 # Plot Total Energy on the first y-axis
 color = 'tab:red'
-ax1.set_xlabel('Bit')
+ax1.set_xlabel('Number of approximate bits')
 ax1.set_ylabel('Total Energy [pJ]', color=color)
-ax1.plot(df['Bit'], df['Total Energy'], color=color, label='Total Energy[pJ]')
+ax1.plot(df['Bit'], df['Total Energy'], color=color, label='Total Energy[pJ]',linewidth=1, linestyle='dashed', marker='o')
 ax1.tick_params(axis='y', labelcolor=color)
-ax1.yaxis.label.set_size(16)  # Set font size for the y-axis label
+ax1.yaxis.label.set_size(12)  # Set font size for the y-axis label
 
 # Create a second y-axis for Steps
 ax2 = ax1.twinx()
 color = 'tab:blue'
 ax2.set_ylabel('Steps [count]', color=color)
-ax2.plot(df['Bit'], df['Steps'], color=color, label='Steps[count]')
+ax2.plot(df['Bit'], df['Steps'], color=color, label='Steps[count]',linewidth=1, linestyle='dashed', marker='o')
 ax2.tick_params(axis='y', labelcolor=color)
-ax2.yaxis.label.set_size(16)  # Set font size for the y-axis label
+ax2.yaxis.label.set_size(12)  # Set font size for the y-axis label
 
 # Set font size for x-axis label
-ax1.xaxis.label.set_size(16)
+ax1.xaxis.label.set_size(12)
 
 # # Add legends
 # ax1.legend(loc='upper left')
 # ax2.legend(loc='upper right')
 
 # Show the plot
-plt.title('Total energy and compute step count for varying number of approximate bits', fontsize=16)
+plt.title('Total energy and compute step count for varying number of approximate bits', fontsize=12)
 
 # Save the plot to a file
 plt.savefig('stepsplot.png')
